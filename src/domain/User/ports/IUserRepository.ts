6dd92e9dddd1f51b8dba25d1d17e8d55b9441dc1
EvandroGiacomelli <evandrogiacomelli3@gmail.com>
2026-01-14 08:15:09 -0300

    user por

---
+++ src/domain/User/ports/IUserRepository.ts
@@ -0,0 +1,23 @@
+import {User} from "../entity/User";
+import {UserId} from "../entity/vo/UserId";
+import {UserEmail} from "../entity/vo/information/profileInfo/UserEmail";
+import {UserUsername} from "../entity/vo/information/profileInfo/UserUsername";
+
+export interface IUserRepository {
+    save(user: User): Promise<void>;
+    findById(id: UserId): Promise<User | null>;
+    findByEmail(email: UserEmail): Promise<User | null>;
+    findByUserName(userName: UserUsername): Promise<User | null>;
+    list(page: number, limit: number): Promise<UserListResult>;
+    existsById(id: UserId): Promise<boolean>;
+}
+
+export type UserListResult = {
+    data: User[];
+    pagination: {
+        page: number;
+        limit: number;
+        totalItems: number;
+        totalPages: number;
+    };
+};
